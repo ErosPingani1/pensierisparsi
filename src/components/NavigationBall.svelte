c9c5254faf64bb696f8325150d598a7164910477
ErosPingani1 <e.pingani3@gmail.com>
2023-11-30 08:56:09 +0100

    [DEV][WIP] - Navigation ball component creation

---
+++ src/components/NavigationBall.svelte
@@ -0,0 +1,3 @@
+<script></script>
+
+<div class="w-52 h-52 rounded-full bg-gray-900"></div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
+<script>
+    import NavigationBall from "../components/NavigationBall.svelte";
+</script>
+
 <svelte:head>
     <title>pensierisparsi</title>
     <meta name="description" content="Il luogo dei pensieri attorcigliati" />
 </svelte:head>
+
+<section>
+    <NavigationBall />
+</section>
